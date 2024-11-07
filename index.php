a06d851fedbdd5b08bf840f1f7fea57153c37823
mayang-123 <146847983+mayang-123@users.noreply.github.com>
2024-11-06 23:45:19 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>
-		My first Ubuntu Server PHP Deployment
-	</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Information Form</title>
 </head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <body>
-	<p>My first Ubuntu Server PHP Deployment<p>
-<div>
-	<label>Firstname:</label>
-	<input type="text" name="firstname" class="control">
-</div>
-<div>
-	<label>Middle Name:</label>
-    <input type="text" name="middlename" class="control">
-</div>
-<div>
-	<label>Last Name:</label>
-    <input type="text" name="lastname" class="control">
-</div>
-<div>
-	<label>Age:</label>
-    <input type="text" name="age" class="control">
-</div>
-<div>
-	<label>Address:</label>
-    <input type="text" name="address" class="control">
-</div>
+    <h1>Student Information Form</h1>
+    <form action="submit_form.php" method="post">
+        <label for="fname">First Name:</label><br>
+        <input type="text" id="fname" name="fname" required><br><br>
+
+        <label for="mname">Middle Name:</label><br>
+        <input type="text" id="mname" name="mname" required><br><br>
+
+        <label for="lname">Last Name:</label><br>
+        <input type="text" id="lname" name="lname" required><br><br>
+
+        <label for="age">Age:</label><br>
+        <input type="number" id="age" name="age" required><br><br>
+
+        <label for="address">Address:</label><br>
+        <textarea id="address" name="address" rows="3" required></textarea><br><br>
+
+        <label for="course">Course:</label><br>
+        <input type="text" id="course" name="course" required><br><br>
+
+        <label for="section">Section:</label><br>
+        <input type="text" id="section" name="section" required><br><br>
+
+        <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
