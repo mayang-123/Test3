05e512c787289bcc8d0b2f586b8c3da4150e9e1e
mayang-123 <146847983+mayang-123@users.noreply.github.com>
2024-11-06 23:16:30 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		My first Ubuntu Server PHP Deployment
+	</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<body>
+	<p>My first Ubuntu Server PHP Deployment<p>
+<div>
+	<label>Firstname:</label>
+	<input type="text" name="firstname" class="control">
+</div>
+<div>
+	<label>Middle Name:</label>
+    <input type="text" name="middlename" class="control">
+</div>
+<div>
+	<label>Last Name:</label>
+    <input type="text" name="lastname" class="control">
+</div>
+<div>
+	<label>Age:</label>
+    <input type="text" name="age" class="control">
+</div>
+<div>
+	<label>Address:</label>
+    <input type="text" name="address" class="control">
+</div>
+</body>
+</html>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
