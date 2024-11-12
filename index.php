ed43c26b9e6fd607fa756d40991da7a11c7a1ebe
mayang-123 <146847983+mayang-123@users.noreply.github.com>
2024-11-12 14:01:52 +0800

    index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>My First Ubuntu server PHP Deployment</title>
 </head>
 <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <h1>My First Ubuntu server PHP Deployment</h1>
     <form action="submit_form.php" method="post">
         <label for="fname">First Name:</label><br>
@@ -31,6 +32,7 @@
 
         <button type="submit">Submit</button>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
 
